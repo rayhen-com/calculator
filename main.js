326d56b94c046436ff6c35d1eabbb2c3ad128fdd
4132 <121490600+rayhen-com@users.noreply.github.com>
2023-06-22 17:23:13 +0200

    Add clear function

--- main.js
+++ main.js
@@ -13,3 +13,31 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 };
+
+function clearDisplay() {
+    display.textContent = '';
+};
+
+// Variables
+let num1;
+let num2;
+let operator;
+
+// Calls the operating functions
+function operate(num1, num2, operator) {
+
+};
+
+// Numbers
+let display = document.getElementById('display');
+let numbers = document.getElementsByClassName('number');
+let clear = document.getElementById('clear');
+
+// Clear button
+clear.addEventListener("click", function(){
+    clearDisplay();
+});
+
+numbers.addEventListener("click", function(){
+    console.log("test");
+});
